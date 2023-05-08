c83072bab6a52d1ff32e2c03a617495c54f93bc5
SamuelTodd <stodd03@calpoly.edu>
2023-05-08 12:11:33 -0700

    cleanImport function general structure

--- R/import_cleaning.R
+++ R/import_cleaning.R
@@ -5,6 +5,10 @@
 #' @param removeNA a boolean to remove rows with NA values
 #' @param removeDuplicates a boolean to remove duplicate rows
 #' @param cleanNames a boolean to clean the column names using the janitor package
+#' @param delimeter a character representing the delimeter if a text file is being read in
+#'
+#' @importFrom readxl read_excel
+#' @importFrom utils read.table read.csv
 #'
 #' @return A cleaned dataframe
 #'
@@ -12,11 +16,21 @@
 cleanImport <- function(file, type,
                         removeNA = TRUE,
                         removeDuplicates = TRUE,
-                        cleanNames = TRUE) {
+                        cleanNames = TRUE,
+                        delimeter = "\t") {
 
-  cleanData()
+  if(!(type %in% c("csv", "excel", "text"))){
+    stop("Must provide support file type (\"csv\", \"excel\", or \"excel\"")
+  }
 
-  return(NULL)
+  df <- switch(type,
+         "csv" = read.csv(filename),
+         "excel" = read_excel(filename),
+         "text" = read.table(filename, sep = delimeter))
+
+  # cleanData()
+
+  return(df)
 
 }
 
