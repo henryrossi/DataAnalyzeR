22e29c47a9dfb4257cb09efd47af9f11095dee14
SamuelTodd <stodd03@calpoly.edu>
2023-05-09 00:52:59 -0700

    completed intial functions with minimal testing

--- R/import_cleaning.R
+++ R/import_cleaning.R
@@ -1,49 +1,82 @@
-#' Loads a file given a format, with optional treatment of NA values
+#' Loads in a file and cleans it according to user specification, returning the result in a dataframe
 #'
-#' @param file The name of the file to be imported
-#' @param file The type of the file ("csv" = (.csv), "excel" = (.xls, .xlsx) "text" = (txt))
+#' @param fileName The name of the file to be imported
+#' @param sheetName The name of the sheet to be opened (only for xls and xlsx files)
+#' @param headerRow a boolean if the file contains a header row (only for txt files)
 #' @param removeNA a boolean to remove rows with NA values
 #' @param removeDuplicates a boolean to remove duplicate rows
 #' @param cleanNames a boolean to clean the column names using the janitor package
-#' @param delimeter a character representing the delimeter if a text file is being read in
 #'
-#' @importFrom readxl read_excel
+#' @importFrom readxl read_excel read_xlsx
 #' @importFrom utils read.table read.csv
+#' @importFrom stringr str_subset
 #'
 #' @return A cleaned dataframe
 #'
 #' @export
-cleanImport <- function(file, type,
+cleanImport <- function(fileName,
+                        sheetName = 1,
+                        headerRow = TRUE,
                         removeNA = TRUE,
                         removeDuplicates = TRUE,
-                        cleanNames = TRUE,
-                        delimeter = "\t") {
+                        cleanNames = TRUE) {
 
-  if(!(type %in% c("csv", "excel", "text"))){
-    stop("Must provide support file type (\"csv\", \"excel\", or \"excel\"")
+  # get the type of the file from the file extension
+  type <- sub(".*\\.([^\\.]*)$", "\\1", fileName)
+
+  # stop and provide an error if the file type is not supported
+  if(!(type %in% c("csv", "xls", "xlsx", "txt"))){
+    stop("Must provide supported file type (.csv, .txt, .xls, or .xlsx)")
   }
 
+  # depending on the type of the file, load in the data
   df <- switch(type,
-         "csv" = read.csv(filename),
-         "excel" = read_excel(filename),
-         "text" = read.table(filename, sep = delimeter))
+         "csv" = read.csv(fileName),
+         "txt" = read.table(fileName, header = headerRow),
+         "xls" = read_excel(fileName, sheet = sheetName),
+         "xlsx" = read_xlsx(fileName, sheet = sheetName))
 
-  # cleanData()
+  # pass in the new dataframe into the data cleaning helper function
+  df <- cleanData(df, removeNA, removeDuplicates, cleanNames)
 
-  return(df)
+  # return the final cleaned dataframe
+  return(as.data.frame(df))
 
 }
 
-#' Cleans the data in the data frame
+#' Cleans the data in the data frame according to user specification
 #'
 #' @param df a dataframe to be cleaned
 #' @param removeNA a boolean to remove rows with NA values
 #' @param removeDuplicates a boolean to remove duplicate rows
 #' @param cleanNames a boolean to clean the column names using the janitor package
 #'
+#' @importFrom tidyr drop_na
+#' @importFrom dplyr distinct
+#' @importFrom janitor clean_names
+#'
 #' @return A cleaned dataframe
 cleanData <- function(df, removeNA, removeDuplicates, cleanNames) {
 
-  return(NULL)
+  # if the user requested for NAs to be removed
+  if(removeNA == TRUE){
+    df <- df |>
+      drop_na()
+  }
+
+  # if the user requested duplicates to be removed
+  if(removeDuplicates == TRUE){
+    df <- df |>
+      distinct(.keep_all = TRUE)
+  }
+
+  # if the user requested for names to be cleaned
+  if(cleanNames == TRUE){
+    df <- df |>
+      clean_names()
+  }
+
+  # returned the cleaned dataframe
+  return(df)
 
 }
--- tests/testthat/test-cleanImport.R
+++ tests/testthat/test-cleanImport.R
@@ -1,3 +1,39 @@
-test_that("cleanImport works", {
-  f <- cleanImport("", type = "")
+test_that("cleanImport works with csv", {
+
+  ages_csv <- cleanImport(fileName = "ages.csv")
+
+  expect_equal(class(ages_csv), "data.frame")
+  expect_equal(nrow(ages_csv), 6)
+  expect_equal(ncol(ages_csv), 2)
+
+})
+
+test_that("cleanImport works with txt", {
+
+  ages_txt <- cleanImport(fileName = "ages.txt")
+
+  expect_equal(class(ages_txt), "data.frame")
+  expect_equal(nrow(ages_txt), 6)
+  expect_equal(ncol(ages_txt), 2)
+
+})
+
+test_that("cleanImport works with xls", {
+
+  ages_xls <- cleanImport(fileName = "ages.xls", sheet = "ages")
+
+  expect_equal(class(ages_xls), "data.frame")
+  expect_equal(nrow(ages_xls), 6)
+  expect_equal(ncol(ages_xls), 2)
+
+})
+
+test_that("cleanImport works with xlsx", {
+
+  ages_xlsx <- cleanImport(fileName = "ages.xlsx", sheet = "ages")
+
+  expect_equal(class(ages_xlsx), "data.frame")
+  expect_equal(nrow(ages_xlsx), 6)
+  expect_equal(ncol(ages_xlsx), 2)
+
 })
